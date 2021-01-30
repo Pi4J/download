97fa6d978200d96a2780157b25ddceeca8d5a2c1
Robert Savage <robert@pi4j.com>
2021-01-30 00:25:22 -0500

    testing

--- .build/build-docker.sh
+++ .build/build-docker.sh
@@ -4,7 +4,6 @@ set -e
 #----------------------------------------
 # BUILD API REPO INDICES (using Docker)
 #----------------------------------------
-docker run --rm \
-           --volume ~/.gnupg/pi4j.key:/gnupg/pi4j.key \
-           --volume $(pwd):/build \
-           pi4j/pi4j-builder-repo:latest .build/build.sh
+cp ~/.gnupg/pi4j.key .   # copy local Pi4J PGP privat key to to use during build
+docker run --rm --volume $(pwd):/build pi4j/pi4j-builder-repo:latest .build/build.sh
+rm pi4j.key              # remove local copy of Pi4J PGP private key
